f6ebb7d8cddbea6b7ef0575ae27190915ada32e9
Brittany Crocker <brittany.crocker@outlook.com>
2020-12-03 21:19:15 +1100

    final server, pre database creation

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-let posts = [{text: "Awesome Park", url: "https://maps.randwick.nsw.gov.au/public/images/facilities/Baker-Park-768x250.jpg"}]
+let posts = [{text: "Awesome Park", url: "https://maps.randwick.nsw.gov.au/public/images/facilities/Baker-Park-768x250.jpg", location: 'Baker Park, Sydney AU'}, {text: "Favourite spot!", url: "https://www.ausleisure.com.au/images/ausleisure/files/_news-main/Randwick_Environmental_Park_wetlands.jpg", location: 'Environment Park, Sydney AU' }]
 
 app.use(cors())
 app.use(bodyParser.json())
